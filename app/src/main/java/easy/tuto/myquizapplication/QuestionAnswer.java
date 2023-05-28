83699916ea5662987d561d9a62c8803405015d03
jaxon93 <jaxon932002@gmail.com>
2023-05-29 08:13:21 +0900

    Initial commit

---
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "What is the longest river in the world?",
+            "What is the largest continent in size?",
+            "Which country is the footballer Cristiano Ronaldo from",
+            "Who is the CEO of Amazon?"
+    };
+
+    public static String choices[][] = {
+            {"Amazon River","Nile","Yellow River","Congo River"},
+            {"Asia","Africa","Europe","North America"},
+            {"Spain","Brazil","Uruguay","Portugal"},
+            {"Jeff Bezos","Tim Cook","Mark Zuckerberg","Elon Musk"}
+    };
+
+    public static String correctAnswers[] = {
+            "Nile",
+            "Asia",
+            "Portugal",
+            "Jeff Bezos"
+    };
+
+}
